2a1be5f884e78b2dde82a46ab107e14750c86469
keshu-bara <keshavbarawal4321@gmail.com>
2025-02-22 17:33:03 +0530

    Changes for vercel3

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-is_tuxed2w5ei2tfwx6=2lf!_e55%kl=q_m^0m2a#2lykeq*8-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
