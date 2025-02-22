86854a6189307871a9e2a9d27cab7136337d0e62
keshu-bara <keshavbarawal4321@gmail.com>
2025-02-22 17:21:23 +0530

    Changes for vercel

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-is_tuxed2w5ei2tfwx6=2lf!_e55%kl=q_m^0m2a#2lykeq*8-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
 
 # Application definition
@@ -126,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
